f31cc98b66affbdddd5e1e31c31b99918ed072f6
Daniel Garcia <daniel.garcia@mindcurv.com>
2024-03-06 21:50:52 +0100

    Add new example

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,23 +1,27 @@
-import { defineConfig } from "cypress"
-import createBundler from "@bahmutov/cypress-esbuild-preprocessor"
-import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor"
-import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild"
+import { defineConfig } from "cypress";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor/src";
+import tasks from "./cypress/support/tasks";
 
-async function setupNodeEvents( on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions): Promise<Cypress.PluginConfigOptions> {
-  await addCucumberPreprocessorPlugin(on, config)
-
-  on("file:preprocessor",  createBundler({
-      plugins: [createEsbuildPlugin(config)],
-    })
+async function setupNodeEvents(
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
+): Promise<Cypress.PluginConfigOptions> {
+  await addCucumberPreprocessorPlugin(on, config);
+  on(
+    "file:preprocessor",
+    createBundler({ plugins: [createEsbuildPlugin(config)] })
   );
-
+  on("task", tasks);
   return config;
 }
 
 export default defineConfig({
   e2e: {
-    specPattern: "./cypress/e2e/**/*.feature",
+    setupNodeEvents,
+    specPattern: "./cypress/e2e/**/*.{feature,features}",
     supportFile: "./cypress/support/e2e.ts",
-    setupNodeEvents
-  }
-})
+    baseUrl: "https://the-internet.herokuapp.com",
+  },
+});
--- cypress/e2e/login/features/login.feature
+++
@@ -1,7 +0,0 @@
-Feature: Login
-
-    @login
-    Scenario: User logs in the application
-        Given the user navigates to login page
-        When the user enters valid credentials
-        Then the user is logged in
